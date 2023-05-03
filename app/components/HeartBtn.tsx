9991d40700a987a0c0f908c47dcbeeead39a120a
Earl A. Villapaz <jasonburning16@gmail.com>
2023-05-03 22:21:03 +0800

    Still working on the UI & func of the listing page

--- app/components/HeartBtn.tsx
+++ app/components/HeartBtn.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
 
 interface HeartBtnProps {
@@ -12,8 +13,16 @@ const HeartBtn: React.FC<HeartBtnProps> = ({
     listingId,
     currentUser
 }) => {
+    const hasFavorited = false;
+    
+    const toggleFavorited = () => {
+
+    };
     return ( 
-        <div>HeartBtn</div>
+        <div onClick={toggleFavorited} className="relative hover:opacity-80 transition cursor-pointer">
+            <AiOutlineHeart size={25} className="fill-white absolute -top-[2px] -right-[2px]" />
+            <AiFillHeart size={22} className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'} />
+        </div>
     );
 }
  
