e3e93408f6d7f8dcadf374da47d29e3fbc06bd23
Earl A. Villapaz <jasonburning16@gmail.com>
2023-05-06 19:04:40 +0800

    sucess on adding the first img on the Listing View Route

--- app/components/Listings/ListingHeader.tsx
+++ app/components/Listings/ListingHeader.tsx
@@ -1,8 +1,52 @@
 "use client";
 
-const ListingHeader = () => {
+import Image from "next/image";
+
+import { SafeUser } from "@/app/types";
+import useCountries from "@/app/hooks/useCountries";
+import Headings from "../Headings";
+import HeartBtn from "../HeartBtn";
+
+interface ListingHeaderProps {
+    id: string;
+    title: string;
+    locationValue: string;
+    imgSrc: string;
+    currentUser?: SafeUser | null;
+}
+
+const ListingHeader: React.FC<ListingHeaderProps> = ({
+    id,
+    title,
+    locationValue,
+    imgSrc,
+    currentUser,
+}) => {
+    const { getByValues } = useCountries();
+
+    const location = getByValues(locationValue);
     return (  
-        <div>ListingHeader</div>
+        <>
+            <Headings 
+                title={title}
+                subTitle={`${location?.region}, ${location?.label}`}
+            />
+
+            <div className="w-full h-[60vh] relative overflow-hidden rounded-xl">
+                <Image 
+                    src={imgSrc}
+                    alt="Image"
+                    fill
+                    className="object-cover w-full"
+                />
+                <div className="absolute top-5 right-5">
+                    <HeartBtn 
+                        listingId={id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
  
